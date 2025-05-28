8783367472f39d6160ada3b0e40dffa2d5125d58
User <User@WINDOWS-PGPIVGC>
2025-05-28 16:35:09 +0900

    あ

--- silver/src/jp/ssie/ocjp/Animal.java
+++ silver/src/jp/ssie/ocjp/Animal.java
@@ -15,6 +15,6 @@ public class Animal {
 	}
 	
 	public void showAnimal() {
-		System.out.println("宮田竜希" + name + "で" + voice + "と鳴きます");
+		System.out.println("宮田" + name + "で" + voice + "と鳴きます");
 	}
 }
